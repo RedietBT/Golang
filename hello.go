baf750ff4209cfd2d3c32da5c9a0539003aa0681
unknown <redietberhanu2@gmail.com>
2025-08-20 22:46:16 +0300

    using constsnts with go

--- hello.go
+++ hello.go
@@ -5,6 +5,10 @@ import "fmt"
 func main() {
 	var name string = "Go Programmer"
 	age := 30
+	const Pi = 3.14
+	const greeting = "Hello, Go!"
+	fmt.Println(Pi)
+	fmt.Println(greeting)
 	fmt.Println("Name: ", name, "Age: ", age)
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a simple Go program.")
