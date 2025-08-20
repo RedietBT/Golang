869b8559f8a5f20a81e01aa97478723d0836c8fc
unknown <redietberhanu2@gmail.com>
2025-08-20 22:42:36 +0300

    creating a variable with go

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var name string = "Go Programmer"
+	age := 30
+	fmt.Println("Name: ", name, "Age: ", age)
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a simple Go program.")
 }
