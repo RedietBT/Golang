cfd4342bc2bdc8b2f35fa61faa7671585b312c1f
unknown <redietberhanu2@gmail.com>
2025-12-16 17:14:20 +0300

    database_connectivity.go

---
+++ database_connectivity.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		fmt.Println("Error connecting to SQLite:", err)
+		return
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error pinging SQLite database:", err)
+		return
+	}
+	fmt.Println("Successfully connected to SQLite database")
+}
