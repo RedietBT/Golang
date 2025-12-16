e6496d878190c54cfa8a8cc9f87e6eddd5a9b142
unknown <redietberhanu2@gmail.com>
2025-12-16 12:35:40 +0300

    channels.go

---
+++ channels.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func sendMessage(ch chan string){
+	
+}
+
+func channels() {
+	ch := make(chan int, 3)
+
+	ch <- 1
+	ch <- 2
+	ch <- 3
+
+	close(ch)
+	
+	for val := range ch {
+		fmt.Println(val)
+	}
+}
