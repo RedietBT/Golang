42d77f0e074f2ef9735e223f2d195bdb60c1a8ed
unknown <redietberhanu2@gmail.com>
2025-12-16 15:43:49 +0300

    json_xml.go adding xmls

--- json_xml.go
+++ json_xml.go
@@ -2,6 +2,7 @@ package main
 import (
 	"fmt"
 	"encoding/json"
+	"encoding/xml"
 )
 
 type Person struct {
@@ -10,7 +11,13 @@ type Person struct {
 	Email string `json:"email"`
 }
 
-func main() {
+type BookXML struct {
+	Title  string `xml:"title"`
+	Author string `xml:"author"`
+	Year   int    `xml:"year"`
+}
+
+func jsons() {
 	data := `{"name": "Alice", "age": 30, "email": "alice@example.com"}`
 
 	var person Person
@@ -23,4 +30,32 @@ func main() {
 
 	fmt.Println("JSON DATA.")
 	fmt.Printf("Name: %s, Age: %d, Email: %s\n", person.Name, person.Age, person.Email)
+}
+
+func xmls() {
+	xmldata := `<Book>
+	<title>The Go Programming Language</title>
+	<author>Alan A. A. Donovan</author>
+	<year>2015</year>
+</Book>`
+
+	var book BookXML
+	err := xml.Unmarshal([]byte(xmldata), &book)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Parsed XML DATA.")
+	fmt.Printf("Title: %s, Author: %s, Year: %d\n", book.Title, book.Author, book.Year)
+	encodedData, err := xml.MarshalIndent(book, "", " ")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Re-encoded XML DATA.")
+	fmt.Println(string(encodedData))
 }
